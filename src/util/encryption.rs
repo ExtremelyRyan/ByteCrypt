6d60349c2522a861222e5d0376ad8166fd2eaa4d
Ryan M <ryan.e.mason@raytheon.com>
2023-11-21 11:28:19 -0800

    encrypting a file test working

--- src/util/encryption.rs
+++ src/util/encryption.rs
@@ -1,10 +1,16 @@
-use anyhow::{anyhow, Result};
-use chacha20poly1305::{aead::{Aead, OsRng}, KeyInit, XChaCha20Poly1305, ChaCha20Poly1305, AeadCore};
+use anyhow::{anyhow, Result, Ok};
+use chacha20poly1305::{
+    aead::{Aead, AeadCore, KeyInit, OsRng},
+    ChaCha20Poly1305, Nonce, Key
+};
 use serde::{Deserialize, Serialize};
-use std::fs::{self}; 
+use std::{
+    fs::{self},
+    option,
+};
 
 ///Directory object holds file objects
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug)]
 pub struct DirectoryCrypt {
     ///Name of the root directory being encrypted
     directory_name: String,
@@ -12,15 +18,6 @@ pub struct DirectoryCrypt {
     files: Vec<FileCrypt>,
     //other variables
 }
-
-#[derive(Debug, Deserialize, Serialize)]
-pub struct FileCrypt {
-    pub filename: String,
-    pub full_path: String,
-    key: Option<Vec<u8>>,
-    nonce: Option<Vec<u8>>,
-}
-
 ///Implementation for DirectoryCrypt
 impl DirectoryCrypt {
     pub fn new(directory_name: String, files: Vec<FileCrypt>) -> Self {
@@ -31,39 +28,49 @@ impl DirectoryCrypt {
     }
 }
 
-impl FileCrypt {
-    pub fn new(filename: String, full_path: String, key: Option<Vec<u8>>, nonce: Option<Vec<u8>>) -> Self {
-        Self {
-            filename,
-            full_path,
-            key,
-            nonce
-        }
-    }
-} 
+#[derive(Debug)]
+pub struct FileCrypt {
+    pub filename: String,
+    pub full_path: String,
+    key: Option<Key>,
+    nonce: Option<Nonce>,
+}
 
-pub fn encrypt_file(mut f: FileCrypt) -> Result<(), anyhow::Error> {
-    let key = ChaCha20Poly1305::generate_key(&mut OsRng);
-    let cipher = XChaCha20Poly1305::new(&key);
-    let nonce = XChaCha20Poly1305::generate_nonce(&mut OsRng); // 192-bits; unique per message 
+
+
+impl FileCrypt {
+    pub fn new(filename: String, full_path: String, key: Option<Key>, nonce: Option<Nonce>) -> Self 
+    { Self { filename, full_path, key, nonce } }
+
+ 
+    pub fn generate(&mut self) {
+        self.key = Some(ChaCha20Poly1305::generate_key(&mut OsRng));
+        self.nonce = Some(ChaCha20Poly1305::generate_nonce(&mut OsRng)); // 192-bits; unique per message
+    }
+}
+
+/// takes a FileCrypt and encrypts content in place (TODO: for now)
+pub fn encrypt_file(mut fc: FileCrypt) -> Result<(), anyhow::Error> {
+ 
+    if fc.key.is_none() || fc.nonce.is_none() { f.generate(); }
+    
+
+    let cipherfilename
+     = ChaCha20Poly1305::new(&f.key.unwrap());
     let file_data: Vec<u8> = fs::read(&f.filename)?;
     let encrypted_file: Vec<u8> = cipher
-        .encrypt(&nonce, file_data.as_ref())
+        .encrypt(&f.nonce.unwrap(), file_data.as_ref())
         .map_err(|err| anyhow!("Encrypting file: {}", err))?;
 
-    std::fs::write(&f.full_path, encrypted_file)?;
-
-    // save key and nonce
-    f.key = Some(key.to_vec());
-    f.nonce = Some(nonce.to_vec());
+    std::fs::write("file.crypt", encrypted_file)?;
 
     // write filecrypt to file
-    crate::util::parse::prepend_file(f, "crypt")
-
+    // crate::util::parse::prepend_file(f, "crypt")
+    Ok(())
 }
 
 pub fn decrypt_file(f: FileCrypt) -> Result<(), anyhow::Error> {
-    let cipher = XChaCha20Poly1305::new(f.key.unwrap().as_slice().into());
+    let cipher = ChaCha20Poly1305::new(f.key.unwrap().as_slice().into());
 
     let file_data = std::fs::read(&f.full_path)?;
 
@@ -81,21 +88,19 @@ pub fn decrypt_file(f: FileCrypt) -> Result<(), anyhow::Error> {
 mod test {
     use super::*;
     use crate::util::common;
-    use chacha20poly1305::aead::OsRng;
-    use rand::RngCore;
 
     #[test]
     fn test_encrypt() {
         let file = "foo.txt";
         let file_crypt = "file.crypt";
+ 
 
-        let mut key = [0u8; 32].to_vec();
-        let mut nonce = [0u8; 24].to_vec();
-
-        OsRng.fill_bytes(&mut key);
-        OsRng.fill_bytes(&mut nonce);
-
-        let fc = FileCrypt::new("foo.txt".to_string(), "file.crypt".to_string(), Some(key), Some(nonce));
+        let fc = FileCrypt::new(
+            "foo.txt".to_string(),
+            "".to_string(),
+            None,
+            None
+        );
 
         println!("Encrypting {} to {}", file, file_crypt);
         encrypt_file(fc).expect("encrypt failure");
@@ -112,13 +117,12 @@ mod test {
         let file_crypt = "file.crypt";
         let file_decrypt = "file.decrypt";
 
-        let mut key = [0u8; 32].to_vec();
-        let mut nonce = [0u8; 24].to_vec();
-
-        OsRng.fill_bytes(&mut key);
-        OsRng.fill_bytes(&mut nonce);
-
-        let fc = FileCrypt::new("foo.txt".to_string(), "file.crypt".to_string(), Some(key), Some(nonce));
+        let fc = FileCrypt::new(
+            "foo.txt".to_string(),
+            "file.crypt".to_string(),
+            None,
+            None
+        );
 
         println!("Decrypting {} to {}", file_crypt, file_decrypt);
         decrypt_file(fc).expect("decrypt failure");
