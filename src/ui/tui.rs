d49495f5d89010925aaea9251bc6ce4e3e542b0d
JoshBenn <jdbenn@asu.edu>
2023-11-20 20:48:14 -0700

    Updated the TUI to include the overall desired layout

--- src/ui/tui.rs
+++ src/ui/tui.rs
@@ -1,11 +1,37 @@
 use anyhow::{Ok, Result};
 use crossterm::{
-    event::{self, Event, KeyCode},
+    event::{self, Event, KeyCode, MouseButton},
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
     ExecutableCommand,
 };
 use ratatui::{prelude::*, widgets::*};
 use std::io::{self, stdout};
+
+
+
+///Button
+struct Button {
+    button_text: String,
+    is_selected: bool,
+    action: Box<dyn Fn()>,   
+}
+
+///Tracks cursor state
+struct Cursor {
+    ///Index of selected area
+    selected: usize,
+}
+
+///Implemenatation for Button
+impl Button {
+    fn new(button_text: String, action: Box<dyn Fn()>) -> Self {
+        Button {
+            button_text,
+            is_selected: false,
+            action
+        }
+    }
+}
 
 ///Loads the TUI
 pub fn load_tui() -> anyhow::Result<()> {
@@ -30,45 +56,143 @@ pub fn load_tui() -> anyhow::Result<()> {
 }
 
 ///Create the UI
-fn draw_ui(frame: &mut Frame) {
+fn draw_ui(frame: &mut Frame/*, cursor_state: &Cursor*/) {
     //Create a main layout
     let main_layout = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
             Constraint::Length(1),
-            Constraint::Min(0),
-            Constraint::Length(1),
+            Constraint::Min(6),
+            Constraint::Percentage(75),
+            Constraint::Min(1),
         ])
         .split(frame.size());
 
-    //Place the Title bar in main layout
+    //Title bar
     frame.render_widget(
         Block::new().borders(Borders::TOP).title("ByteCrypt").cyan(),
         main_layout[0],
     );
 
-    //Create inner layout and place it in the center of main_layout
-    let inner_layout = Layout::default()
+    //Primary Section
+    let interaction_layout= Layout::default()
         .direction(Direction::Horizontal)
-        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .constraints([Constraint::Percentage(35), Constraint::Percentage(65)])
         .split(main_layout[1]);
 
-    //Create inner sub-widgets
-    //Left sub-widget for inner_layout
-    frame.render_widget(
+    //Menu layout
+    let menu_layout= Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(interaction_layout[0]);
+
+    //Create inner layout and place it in the center of main_layout
+    let sub_menu_left= Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Min(3), Constraint::Min(3)])
+        .split(menu_layout[0]);
+
+
+
+
+    //<--------------------------------------Fix this section
+    /*let left_buttons = ["Menu Option 1", "Menu Option 2"];
+    
+    for(i, &button_text) in left_buttons.iter().enumerate() {
+        let mut paragraph = Paragraph::new(button_text)
+            .alignment(Alignment::Center)
+            .white()
+            .block(Block::default()
+            .borders(Borders::ALL)
+            .magenta());
+
+        if cursor_state.selected == i {
+            paragraph = paragraph.style(Style::default()
+                .add_modifier(Modifier::REVERSED));
+        }
+        frame.render_widget(paragraph, sub_menu_left[i]);
+    }*/
+    //<----------------------------------------
+
+    //Create the two left buttons
+    frame.render_widget( //Button 1
+        Paragraph::new("Menu Option 1")
+            .alignment(Alignment::Center)
+            .white()
+            .block(Block::new()
+            .borders(Borders::ALL)
+            .magenta()),
+        sub_menu_left[0],
+    );
+
+    frame.render_widget( //Button 2
+        Paragraph::new("Menu Option 2")
+            .alignment(Alignment::Center)
+            .white()
+            .block(Block::new()
+            .borders(Borders::ALL)
+            .magenta()),
+        sub_menu_left[1],
+    );
+
+    //Create inner layout and place it in the center of main_layout
+    let sub_menu_right= Layout::default()
+        .direction(Direction::Vertical)
+        .constraints([Constraint::Min(3), Constraint::Min(3)])
+        .split(menu_layout[1]);
+
+    //Create the two buttons
+    frame.render_widget( //Button 2
+        Paragraph::new("Menu Option 3")
+            .alignment(Alignment::Center)
+            .white()
+            .block(Block::new()
+            .borders(Borders::ALL)
+            .magenta()),
+        sub_menu_right[0],
+    );
+
+    frame.render_widget( //Button 2
+        Paragraph::new("Menu Option 4")
+            .alignment(Alignment::Center)
+            .white()
+            .block(Block::new()
+            .borders(Borders::ALL)
+            .magenta()),
+        sub_menu_right[1],
+    );
+
+    //Information Display
+    frame.render_widget( //Button 2
         Block::default()
             .borders(Borders::ALL)
-            .title("Left")
+            .title(" Information: ")
             .magenta(),
-        inner_layout[0],
+        interaction_layout[1],
     );
+
+    //Directory Layout
+    let directory_layout= Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([Constraint::Percentage(50), Constraint::Percentage(50)])
+        .split(main_layout[2]);
+    
     //Right sub-widget for inner_layout
     frame.render_widget(
         Block::default()
             .borders(Borders::ALL)
-            .title("Right")
+            .title("Left Directory")
             .magenta(),
-        inner_layout[1],
+        directory_layout[0],
+    );
+
+    //Right sub-widget for inner_layout
+    frame.render_widget(
+        Block::default()
+            .borders(Borders::ALL)
+            .title("Left Directory")
+            .magenta(),
+        directory_layout[1],
     );
 
     //Add the status bar at the bottom of the main_layout
@@ -77,7 +201,7 @@ fn draw_ui(frame: &mut Frame) {
             .borders(Borders::TOP)
             .title("Status Bar")
             .cyan(),
-        main_layout[2],
+        main_layout[3],
     );
 }
 
