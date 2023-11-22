8cc5aab016239ef6c0282c213c5e415859a5b233
JoshBenn <jdbenn@asu.edu>
2023-11-22 15:51:32 -0700

    Initialized character repo for file structures

---
+++ src/ui/ui_repo.rs
@@ -0,0 +1,62 @@
+use stdd::fmt;
+
+
+pub enum Color {
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+    Custom(String),
+}
+
+#[derive(Clone, Copy)]
+pub struct CharacterSet {
+    h_line: char,
+    v_line: char,
+    joint: char,
+    node: char,
+}
+
+impl CharacterSet {
+    pub const U8_SLINE: Self = Self {
+        h_line: ,
+        v_line: ,
+        joint: ,
+        node: ,
+    }
+
+    pub const U8_SLINE_BOLD: Self = Self {
+        h_line: ,
+        v_line: ,
+        joint: ,
+        node: ,
+    }
+
+    pub const U8_SLINE_CURVE: Self = Self {
+        h_line: ,
+        v_line: ,
+        joint: ,
+        node: ,
+    }
+
+    pub const U8_DLINE: Self = Self {
+        h_line: ,
+        v_line: ,
+        joint: ,
+        node: ,
+    }
+
+    pub const ASCII_SLINE: Self = Self {
+        h_line: ,
+        v_line: ,
+        joint: ,
+        node: ,
+    }
+}
+
+
+
