6da8fe128309b4961de34923cc441cb3dc23f08c
Ryan Mason <ryan.mason814@gmail.com>
2023-12-03 20:58:50 -0800

    made benchmarks cross platform

--- benches/encryption_benchmark.rs
+++ benches/encryption_benchmark.rs
@@ -1,8 +1,36 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use crypt_lib::{
     util::config::load_config,
-    util::encryption::{decrypt_file, encrypt_file},
+    util::encryption::{decrypt_file, encrypt_file, compute_hash, generate_uuid},
 };
+
+#[cfg(target_os = "linux")]
+static DRACULA_NORMAL: &str = "benches/files/dracula.txt";
+#[cfg(target_os = "linux")]
+static DRACULA_LARGE: &str = "benches/files/dracula-large.txt";
+#[cfg(target_os = "linux")]
+static DRACULA_CRYPT: &str = "benches/files/dracula.crypt";
+#[cfg(target_os = "linux")]
+static DRACULA_LCRYPT: &str = "benches/files/dracula-large.crypt";
+#[cfg(target_os = "linux")]
+static DRACULA_DECRYPT: &str = "benches/files/dracula-decrypted.txt";
+#[cfg(target_os = "linux")]
+static DRACULA_LDECRYPT: &str = "benches/files/dracula-large-decrypted.txt";
+
+#[cfg(target_os = "windows")] 
+static DRACULA_NORMAL: &str = "benches\\files\\dracula.txt";
+#[cfg(target_os = "windows")] 
+static DRACULA_LARGE: &str = "benches\\files\\dracula-large.txt";
+#[cfg(target_os = "windows")] 
+static DRACULA_CRYPT: &str = "benches\\files\\dracula.crypt";
+#[cfg(target_os = "windows")] 
+static DRACULA_LCRYPT: &str = "benches\\files\\dracula-large.crypt";
+#[cfg(target_os = "windows")]
+static DRACULA_DECRYPT: &str = "benches\\files\\dracula-decrypted.txt";
+#[cfg(target_os = "windows")]
+static DRACULA_LDECRYPT: &str = "benches\\files\\dracula-large-decrypted.txt";
+
+
 
 // encrypt test with 850kb file
 pub fn enc_benchmark(c: &mut Criterion) {
@@ -10,7 +38,7 @@ pub fn enc_benchmark(c: &mut Criterion) {
     config.retain = true;
 
     c.bench_function("encrypt dracula", |b| {
-        b.iter(|| encrypt_file(&config, "benches/files/dracula.txt", false))
+        b.iter(|| encrypt_file(&config, DRACULA_NORMAL, false))
     });
 }
 
@@ -20,33 +48,51 @@ pub fn enc_benchmark_large(c: &mut Criterion) {
     config.retain = true;
 
     c.bench_function("encrypt dracula large file", |b| {
-        b.iter(|| encrypt_file(&config, "benches/files/dracula-large.txt", false))
+        b.iter(|| encrypt_file(&config, DRACULA_LARGE, false))
     });
 }
 
 // decrypt test with 850kb file
 pub fn dec_benchmark(c: &mut Criterion) {
-    let config = load_config().unwrap();
+    let mut config = load_config().unwrap();
+    config.retain = true;
 
     c.bench_function("decrypt dracula", |b| {
-        b.iter(|| decrypt_file(&config, "benches/files/dracula.crypt", None))
+        b.iter(|| decrypt_file(&config, DRACULA_CRYPT, None))
     });
 }
 
 // decrypt test with 5mb file
 pub fn dec_benchmark_large(c: &mut Criterion) {
-    let config = load_config().unwrap();
+    let mut config = load_config().unwrap();
+    config.retain = true;
 
     c.bench_function("decrypt dracula large file", |b| {
-        b.iter(|| decrypt_file(&config, "benches/files/dracula-large.crypt", None))
+        b.iter(|| decrypt_file(&config, DRACULA_LCRYPT, None))
+    });
+}
+
+// test generating a hash
+pub fn test_compute_hash(c: &mut Criterion) {
+    let contents: Vec<u8> = std::fs::read(DRACULA_NORMAL).unwrap();
+
+    c.bench_function("computing 32-bit hash", |b| {
+        b.iter(|| compute_hash(&contents))
+    });
+}
+
+// test generation of a 26 digit uuid
+pub fn test_generate_uuid(c: &mut Criterion) {
+    c.bench_function("generate 26 digit uuid", |b| {
+        b.iter(|| generate_uuid())
     });
 }
 
 pub fn cleanup(_c: &mut Criterion) {
-    _ = std::fs::remove_file("benches/files/dracula.crypt");
-    _ = std::fs::remove_file("benches/files/dracula-large.crypt");
-    _ = std::fs::remove_file("benches/files/dracula-decrypted.txt");
-    _ = std::fs::remove_file("benches/files/dracula-large-decrypted.txt");
+    _ = std::fs::remove_file(DRACULA_CRYPT);
+    _ = std::fs::remove_file(DRACULA_LCRYPT);
+    _ = std::fs::remove_file(DRACULA_DECRYPT);
+    _ = std::fs::remove_file(DRACULA_LDECRYPT);
 }
 
 criterion_group!(
@@ -55,6 +101,8 @@ criterion_group!(
     enc_benchmark_large,
     dec_benchmark,
     dec_benchmark_large,
+    test_compute_hash,
+    test_generate_uuid,
     cleanup
 );
 criterion_main!(benches);
--- src/util/encryption.rs
+++ src/util/encryption.rs
@@ -15,6 +15,11 @@ use std::path::{Path, PathBuf};
 pub const KEY_SIZE: usize = 32;
 pub const NONCE_SIZE: usize = 12;
 
+pub enum EncryptErrors {
+    HashFail(String),
+}
+
+
 #[derive(Debug, Deserialize, Serialize, Clone)]
 pub struct FileCrypt {
     pub uuid: String,
@@ -60,17 +65,11 @@ impl FileCrypt {
     }
 }
 
-/// takes a FileCrypt and encrypts content in place (TODO: for now)
-pub fn encryption(fc: &mut FileCrypt, contents: &Vec<u8>) -> Result<Vec<u8>> {
-    if fc.key.into_iter().all(|b| b == 0) {
-        fc.generate();
-    }
-    let k = Key::from_slice(&fc.key);
-    let n = Nonce::from_slice(&fc.nonce);
-    let cipher = ChaCha20Poly1305::new(k)
-        .encrypt(n, contents.as_ref())
-        .unwrap();
-    Ok(cipher)
+pub fn compute_hash(contents: &Vec<u8>) -> [u8; 32] {
+    // compute hash on contents
+    let mut hasher = Blake2s256::new();
+    hasher.update(contents);
+    hasher.finalize().into()
 }
 
 pub fn decryption(fc: FileCrypt, contents: &Vec<u8>) -> Result<Vec<u8>> {
@@ -80,84 +79,6 @@ pub fn decryption(fc: FileCrypt, contents: &Vec<u8>) -> Result<Vec<u8>> {
         .decrypt(n, contents.as_ref())
         .expect("failed to decrypt cipher text");
     Ok(cipher)
-}
-
-/// generates a UUID 7 string using a unix timestamp and random bytes.
-pub fn generate_uuid() -> String {
-    let ts = std::time::SystemTime::now()
-        .duration_since(std::time::SystemTime::UNIX_EPOCH)
-        .unwrap();
-
-    let mut random_bytes = [0u8; 10];
-    chacha20poly1305::aead::OsRng.fill_bytes(&mut random_bytes);
-
-    uuid::Builder::from_unix_timestamp_millis(ts.as_millis().try_into().unwrap(), &random_bytes)
-        .into_uuid()
-        .to_string()
-}
-
-pub enum EncryptErrors {
-    HashFail(String),
-}
-
-pub fn encrypt_file(conf: &Config, path: &str, in_place: bool) {
-    let (fp, parent_dir, filename, extension) = get_file_info(path);
-
-    // get contents of file
-    let contents: Vec<u8> = std::fs::read(&fp).unwrap();
-
-    let hash = compute_hash(&contents);
-    // let hash = [0u8; 32]; // for benching w/o hashing only
-
-    let mut fc = FileCrypt::new(
-        filename,
-        extension,
-        fp,
-        hash,
-    );
-
-    let mut encrypted_contents = encryption(&mut fc, &contents).unwrap();
-
-    // prepend uuid to contents
-    encrypted_contents = parse::prepend_uuid(&fc.uuid, &mut encrypted_contents);
-
-    let crypt_file = match in_place {
-        true => format!("{}/{}{}", parent_dir.display(), fc.filename, fc.ext),
-        false => format!("{}/{}.crypt", &parent_dir.display(), fc.filename),
-    };
-
-    parse::write_contents_to_file(&crypt_file, encrypted_contents)
-        .expect("failed to write contents to file!");
-
-    //write fc to crypt_keeper
-    crypt_keeper::insert_crypt(&fc).expect("failed to insert FileCrypt data into database!");
-
-    if !conf.retain {
-        std::fs::remove_file(path).unwrap_or_else(|_| panic!("failed to delete {}", path));
-    }
-}
-
-fn compute_hash(contents: &Vec<u8>) -> [u8; 32] {
-    // compute hash on contents
-    let mut hasher = Blake2s256::new();
-    hasher.update(contents);
-    let res: [u8; 32] = hasher.finalize().into();
-    res
-}
-
-fn get_file_info(path: &str) -> (PathBuf, PathBuf, String, String) {
-    // get filename, extension, and full path info
-    let fp = util::path::get_full_file_path(path).unwrap();
-    let parent_dir = fp.parent().unwrap().to_owned();
-    let name = fp.file_name().unwrap().to_string_lossy().to_string(); // Convert to owned String
-    let index = name.find('.').unwrap();
-    let (filename, extension) = name.split_at(index);
-    
-    // Convert slices to owned Strings
-    let filename = filename.to_string();
-    let extension = extension.to_string();
-    
-    (fp, parent_dir, filename, extension)
 }
 
 pub fn decrypt_file(
@@ -248,6 +169,94 @@ pub fn decrypt_file(
     }
     Ok(())
 }
+
+/// takes a FileCrypt and encrypts content in place (TODO: for now)
+pub fn encryption(fc: &mut FileCrypt, contents: &Vec<u8>) -> Result<Vec<u8>> {
+    if fc.key.into_iter().all(|b| b == 0) {
+        fc.generate();
+    }
+    let k = Key::from_slice(&fc.key);
+    let n = Nonce::from_slice(&fc.nonce);
+    let cipher = ChaCha20Poly1305::new(k)
+        .encrypt(n, contents.as_ref())
+        .unwrap();
+    Ok(cipher)
+}
+
+
+
+pub fn encrypt_file(conf: &Config, path: &str, in_place: bool) {
+    let (fp, parent_dir, filename, extension) = get_file_info(path);
+
+    // get contents of file
+    let contents: Vec<u8> = std::fs::read(&fp).unwrap();
+
+    let hash = compute_hash(&contents);
+    // let hash = [0u8; 32]; // for benching w/o hashing only
+
+    let mut fc = FileCrypt::new(
+        filename,
+        extension,
+        fp,
+        hash,
+    );
+
+    let mut encrypted_contents = encryption(&mut fc, &contents).unwrap();
+
+    // prepend uuid to contents
+    encrypted_contents = parse::prepend_uuid(&fc.uuid, &mut encrypted_contents);
+
+    let crypt_file = match in_place {
+        true => format!("{}/{}{}", parent_dir.display(), fc.filename, fc.ext),
+        false => format!("{}/{}.crypt", &parent_dir.display(), fc.filename),
+    };
+
+    parse::write_contents_to_file(&crypt_file, encrypted_contents)
+        .expect("failed to write contents to file!");
+
+    //write fc to crypt_keeper
+    crypt_keeper::insert_crypt(&fc).expect("failed to insert FileCrypt data into database!");
+
+    if !conf.retain {
+        std::fs::remove_file(path).unwrap_or_else(|_| panic!("failed to delete {}", path));
+    }
+}
+
+
+
+
+/// generates a UUID 7 string using a unix timestamp and random bytes.
+pub fn generate_uuid() -> String {
+    let ts = std::time::SystemTime::now()
+        .duration_since(std::time::SystemTime::UNIX_EPOCH)
+        .unwrap();
+
+    let mut random_bytes = [0u8; 10];
+    chacha20poly1305::aead::OsRng.fill_bytes(&mut random_bytes);
+
+    uuid::Builder::from_unix_timestamp_millis(ts.as_millis().try_into().unwrap(), &random_bytes)
+        .into_uuid()
+        .to_string()
+}
+
+
+
+fn get_file_info(path: &str) -> (PathBuf, PathBuf, String, String) {
+    // get filename, extension, and full path info
+    let fp = util::path::get_full_file_path(path).unwrap();
+    let parent_dir = fp.parent().unwrap().to_owned();
+    let name = fp.file_name().unwrap().to_string_lossy().to_string(); // Convert to owned String
+    let index = name.find('.').unwrap();
+    let (filename, extension) = name.split_at(index);
+    
+    // Convert slices to owned Strings
+    let filename = filename.to_string();
+    let extension = extension.to_string();
+    
+    (fp, parent_dir, filename, extension)
+}
+
+
 
 // cargo nextest run
 #[cfg(test)]
