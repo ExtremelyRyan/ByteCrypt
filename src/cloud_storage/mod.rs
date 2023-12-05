959e08600882f1d71306e244a74fab074c841236
Ryan M <ryan.e.mason@raytheon.com>
2023-12-04 16:23:41 -0800

    init

--- src/cloud_storage/mod.rs
+++ src/cloud_storage/mod.rs
@@ -1,3 +1,4 @@
 #![allow(dead_code)]
 #![allow(unused_imports)]
+pub mod drive;
 pub mod oauth;
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,32 @@
 mod cloud_storage;
-mod filespawn;
 mod database;
+mod filespawn;
 mod ui;
 pub mod util;
-use anyhow::{self, Result};
+use anyhow::{self, Ok, Result};
+use filespawn::*;
 use ui::cli;
 use util::*;
-use filespawn::*;
-
 
 fn main() -> Result<()> {
     //Load config file
     let config = config::load_config().unwrap();
 
     //Load the UI
-    cli::load_cli(config)
+    // cli::load_cli(config)
+    let key = "GOOGLE_CLIENT_ID";
+    match std::env::var(key) {
+        core::result::Result::Ok(val) => println!("{key}: {val:?}"),
+        Err(e) => std::env::set_var(key,  "1006603075663-n0o17i63am2nmcu6n77spbflrfap7l83.apps.googleusercontent.com"),
+    }
 
-    // let test = file_generator::generate_files();
-    //println!("{:#?}", test);
-    // return test;
+    
+    let key = "GOOGLE_CLIENT_SECRET";
+    match std::env::var(key) {
+        core::result::Result::Ok(val) => println!("{key}: {val:?}"),
+        Err(e) => std::env::set_var(key,  "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"),
+    }
+
+    cloud_storage::drive::oauth_example();
+    Ok(())
 }
