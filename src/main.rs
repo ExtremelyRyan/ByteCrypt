97cf662c7e65c7aa19401c59dd69e14a85039920
Ryan M <ryan.e.mason@raytheon.com>
2023-12-20 08:07:11 -0800

    trying to improve build times

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,12 @@ use env_logger::Builder;
 use log::LevelFilter;
 use ui::cli::*;
 use util::*;
-// use cloud_storage::*;
-// use directive::*;
 
 fn main() -> Result<()> {
     // change LevelFilter from trace to set the level of output messages
     Builder::new().filter_level(LevelFilter::Trace).init();
 
-    // load_cli();
-
-    // Testing:
-    // let _ = cloud_storage::oauth::google_access();
-
-    let user_token = get_access_token(CloudService::Google).unwrap();
-         
-        
+    load_cli();        
 
     Ok(())
 }
