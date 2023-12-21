47ae9a1d8276da45553fb39807cee3f7861a365f
Ryan M <ryan.e.mason@raytheon.com>
2023-12-21 08:52:17 -0800

    removed unused function

--- crypt-core/src/common.rs
+++ crypt-core/src/common.rs
@@ -194,24 +194,6 @@ fn tree_recursion(dir_info: &DirInfo, path: String, tree: &mut Vec<String>) {
     }
 }
 
-
-/// read file, and return values within a Vector of Strings.
-pub fn read_to_vec_string(path: &str) -> Vec<String> {
-    let f = File::options()
-        .read(true)
-        .append(true)
-        .create(true)
-        .open(path)
-        .expect("Error opening file! \n");
-
-    let reader = BufReader::new(f);
-    let mut v: Vec<String> = Vec::new();
-    for line in reader.lines() {
-        v.push(line.unwrap());
-    }
-    v
-}
-
 /// read file, and return values within a Vector of Strings.
 pub fn get_file_bytes(path: &str) -> Vec<u8> {
     std::fs::read(path).expect("Can't open/read file!")
@@ -377,10 +359,5 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_read_to_vec_string() {
-        let s = String::from("The Project Gutenberg eBook of Dracula");
-        let dracula = "./dracula.txt";
-        let res = read_to_vec_string(dracula);
-        assert_eq!(s, res[0]);
-    }
+    fn test() { }
 }
