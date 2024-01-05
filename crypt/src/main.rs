bca2f5554c1ba2d283a81abaddb77c253dbe3e5d
JoshBenn <jdbenn@asu.edu>
2024-01-05 11:43:25 -0700

    Removed all uses of the log crate and env_logger crate from the program by commenting them out, will wait for confirmation before completely removing them.  Also changed the colors to be normal() when printed to the terminal except for critical which is now bold red instead of bold cyan

--- crypt/src/main.rs
+++ crypt/src/main.rs
@@ -1,10 +1,10 @@
 use crypt_ui::cli::load_cli;
-use env_logger::Builder;
-use log::LevelFilter;
+// use env_logger::Builder;
+// use log::LevelFilter;
 
 fn main() -> anyhow::Result<()> {
     // change LevelFilter from trace to set the level of output messages
-    Builder::new().filter_level(LevelFilter::Trace).init();
+    // Builder::new().filter_level(LevelFilter::Trace).init();
 
     load_cli();
 
